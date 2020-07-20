9de44356da6831e0109ad1a88a68d3667ac86e15
KieronMoore <67976013+KieronMoore@users.noreply.github.com>
2020-07-20 13:40:31 -0500

    Add files via upload

---
+++ Kieron_M_Function_Exersice.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func equation(number1: Int , number2: Int) -> Int {
+    
+    let results = number1 + number2
+    
+    return results
+}
+
+print(equation(number1: 5, number2: 25))
